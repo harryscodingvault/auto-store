e0c246d4df19af35582b838fcfcbb3d83102910d
HaroldDRF <95robots@gmail.com>
2022-08-18 00:42:05 -0700

    Login and register form created

--- front-end/src/pages/SignIn/SignIn.tsx
+++ front-end/src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Wrapper } from "./SignIn.style";
+
+import Login from "../../components/Login/Login";
+import Register from "../../components/Register/Register";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const [login, setLogin] = useState(true);
+
+  return (
+    <Wrapper>
+      {login ? (
+        <>
+          <Login />
+          <div className="btn btn-switch" onClick={() => setLogin(false)}>
+            <h5>Want to register?</h5>
+          </div>
+        </>
+      ) : (
+        <>
+          <Register />
+          <div className="btn btn-switch" onClick={() => setLogin(true)}>
+            <h5>Want to login?</h5>
+          </div>
+        </>
+      )}
+    </Wrapper>
+  );
 };
 
 export default SignIn;
