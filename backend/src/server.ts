12173af86458138a2b15efb97d8075806567123b
HaroldDRF <95robots@gmail.com>
2022-09-25 00:34:29 -0700

    backend: passport google auth2 added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,16 +2,34 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import socketioApp from "./socketio/socketio";
+import passport from "passport";
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 const server = http.createServer(app);
-
 socketioApp(server);
 
 app.use(cors());
 
-const port = process.env.PORT || 5000;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callBackURL: "/api/auth/google/callback",
+    },
+    (accessToken: any) => {
+      console.log(accessToken);
+    }
+  )
+);
 
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Getting server at port ${port}`);
 });
